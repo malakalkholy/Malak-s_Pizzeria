bda2b334b8ecd325690ec63e0b66b994b4c24e69
malakalkholy <2300449@eng.asu.edu.eg>
2025-10-15 17:31:50 +0300

    Move index.html to root

---
+++ Myportifolio/Me.html.py
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Personal Site</title>
+  <link href="styles.css" rel="stylesheet" />
+</head>
+<body>
+  <header>
+    <h1>Welcome to My Personal Site!</h1>
+
+    <nav> 
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+
+  </header>
+
+  <main>
+
+<section id="about">
+    <h2>About Me</h2>
+    <img id="myimg" src="blob:https://web.whatsapp.com/fe6d782b-3216-4f9e-9641-02a44e0181d9" alt="Profile Picture" />
+    <p id="myinfo" >Hello! I'm [Malak], a [Computer & Systems engineering student]. I love coding, cooking, and photography. Welcome to my personal site where you can learn more about me and my work.</p>
+</section>
+
+
+<section id="projects">
+    <h2>Projects</h2>
+    <ul>
+        <li><b>-Autonomous Arduino car</b>: A self-driving car using Arduino and sensors.</li>
+        <li><b>-DC power supply</b>: A 10v power supply using electronic circuits.</li>
+        <li><b>-PWM controlled solar tracker</b>: Design & implementation of a pwm controlled solar tracker with no microcontroller.</li>
+        <li><b>-Audio amplifier</b>: Design and implementation of an audio amplifier using LM385 & class AB power amplifier.</li>
+    </ul>
+</section>
+
+
+<section id="contact">
+    <h2>Contact Me</h2>
+    <p>If you'd like to get in touch, feel free to reach out via email at [malakalkholy410@gmail.com] or connect with me on LinkedIn [linkedin.com/in/malak-alkholy-8ab295325].</p>
+</section>
+
+</main>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+  </main>
+  <footer>
+  </footer>
+</body>
+</html>
+
+
